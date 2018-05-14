49eed776c31ba2030495157fd6f8ff2b8e46ab0b
Adrian Jablonski <jablonski.adrian1@gmail.com>
2018-05-13 21:15:14 -0500

    Added skill experience, leveling up, and fight stances

--- js/scenes/Scene1.js
+++ js/scenes/Scene1.js
@@ -3,6 +3,22 @@
 	Then go to http://localhost:8080 
 */
 
+function Hero() {
+    this.name = "Hero";
+    this.power = 3;
+    this.defense = 1;
+    this.health = 15;
+    this.maxhealth = 15;
+    this.powerExp = Math.round((25 + (this.power)) * (this.power) / 1.13767) * (this.power - 1);
+    this.defenseExp = Math.round((25 + (this.defense)) * (this.defense) / 1.13767) * (this.defense - 1);
+    this.healthExp = Math.round((25 + (this.health)) * (this.health) / 1.13767) * (this.health - 1);
+    this.attackStance = "Aggressive";
+    this.nextHealthLevelExp = Math.round(((25 + (this.health + 1)) * (this.health + 1) / 1.13767) * this.health);
+    this.nextPowerLevelExp = Math.round(((25 + (this.power + 1)) * (this.power + 1) / 1.13767) * this.power);
+    this.nextDefenseLevelExp = Math.round(((25 + (this.defense + 1)) * (this.defense + 1) / 1.13767) * this.defense);
+}
+        
+
 class Scene1 extends Phaser.Scene {
     constructor() {
         super({key:"Scene1"});
@@ -25,6 +41,9 @@ class Scene1 extends Phaser.Scene {
         this.load.image('scrollUp', 'assets/sprites/icons/scroll_up_button.gif');
         this.load.image('scrollDown', 'assets/sprites/icons/scroll_down_button.gif');
 
+        this.load.image('radioButton', 'assets/sprites/icons/radiobutton_circle.png');
+
+        // Colors 
         this.neon = "#39FF14"
 
     }
@@ -39,19 +58,34 @@ class Scene1 extends Phaser.Scene {
         this.attackTimer = 200;
 
         // Renders images and sprites to screen
-        this.image = this.add.image(850 / 2, 750 / 2, 'dark-background');
+        this.image = this.add.image(850 / 2, 700 / 2, 'dark-background');
 	    this.image1 = this.add.image(512 / 2, (480 / 2) + 30, 'background');
         this.image2 = this.add.image(325 / 2 + 520, 619 / 2, 'user-interface');
-        this.scrollUp = this.add.image(465, 600, 'scrollUp')
-        this.scrollDown = this.add.image(465, 640, 'scrollDown')
-        
-        this.hero = this.add.sprite(300, 300, 'hero');
+        this.scrollUp = this.add.image(465, 570, 'scrollUp');
+        this.scrollDown = this.add.image(465, 610, 'scrollDown');
+        this.radioButton = this.add.image(552, 530, 'radioButton');
 
-        this.hero.name = "Hero";
-        this.hero.power = 3;
-        this.hero.defense = 3;
-        this.hero.health = 15;
-        this.hero.maxhealth = 15;
+        // this.hero = this.add.sprite(300, 300, 'hero');
+
+        // // Hero character
+        // this.hero.name = "Hero";
+        // this.hero.power = 3;
+        // this.hero.defense = 3;
+        // this.hero.health = 15;
+        // this.hero.maxhealth = 15;
+        // this.hero.powerExp = 0;
+        // this.hero.defenseExp = 0;
+        // this.hero.healthExp = 0;
+        // this.hero.attackStance = "Aggressive";
+
+        // http://www.html5gamedevs.com/topic/32411-extending-phasergameobjectssprite-es6/
+        this.hero = new Hero({
+            scene: this,
+            x: 300,
+            y: 300,
+            key: 'hero',
+        })
+
 
         //create hero and enemy health bar off screen
         this.statusBarHero0 = this.add.image(-30, - 40, 'statusBar0');
@@ -64,11 +98,18 @@ class Scene1 extends Phaser.Scene {
         // this.healthbar.cropEnabled = true;
 
         // shows hero levels on screen
-        this.heroPowerLvText = this.add.text(670, 110, this.hero.power, {font:"24px Ariel", color:this.neon});
-        
-        this.heroDefenseLvText = this.add.text(670, 160, this.hero.defense, {font:"24px Ariel", color:"Blue"});
+        this.heroPowerLvText = this.add.text(650, 112, this.hero.power, {font:"20px Ariel", color:this.neon});
 
-        this.heroHealthLvText = this.add.text(670, 220, this.hero.health, {font:"24px Ariel", color:"Red"});
+        this.heroDefenseLvText = this.add.text(650, 162, this.hero.defense, {font:"20px Ariel", color:"Blue"});
+
+        this.heroHealthLvText = this.add.text(650, 222, this.hero.health, {font:"20px Ariel", color:"Red"});
+
+        // Shows current and next lv hero Exp on screen
+        this.heroPowerExpText = this.add.text(545, 135, `Current Exp: ${this.hero.powerExp}        Next Level: ${this.hero.nextPowerLevelExp}`, {font:"14px Ariel", color:this.neon});
+        
+        this.heroDefenseExpText = this.add.text(545, 190, `Current Exp: ${this.hero.defenseExp}        Next Level: ${this.hero.nextDefenseLevelExp}`, {font:"14px Ariel", color:"Blue"});
+
+        this.heroHealthExpText = this.add.text(545, 245, `Current Exp: ${this.hero.healthExp}        Next Level: ${this.hero.nextHealthLevelExp}`, {font:"14px Ariel", color:"Red"});
 
         // Damage text
         this.heroDamageText = this.add.text(-20, 20, "", {font:"18px 'Ariel Bold'", color:"Red"});
@@ -77,17 +118,21 @@ class Scene1 extends Phaser.Scene {
         // History Log
         this.historyTextHeading = this.add.text(20, 513, "History:", {font:"20px Ariel"});
         this.historyTextLine = this.add.text(10, 528, "-----------------------------------------------")
-        this.historyTextLine1 = this.add.text(40, 545, "Line 1 of history log", {font:"16px Ariel Bold"});
-        this.historyTextLine2 = this.add.text(40, 570, "Line 2 of history log", {font:"16px Ariel"});
-        this.historyTextLine3 = this.add.text(40, 595, "Line 3 of history log", {font:"16px Ariel"});
-        this.historyTextLine4 = this.add.text(40, 620, "Line 4 of history log", {font:"16px Ariel"});
-        this.historyTextLine5 = this.add.text(40, 645, "Line 5 of history log", {font:"16px Ariel"});
-        this.historyTextLine6 = this.add.text(40, 670, "Line 6 of history log", {font:"16px Ariel"});
+        this.historyTextLine1 = this.add.text(30, 545, "", {font:"16px 'Ariel Bold'"});
+        this.historyTextLine2 = this.add.text(30, 570, "", {font:"16px Ariel"});
+        this.historyTextLine3 = this.add.text(30, 595, "", {font:"16px Ariel"});
+        this.historyTextLine4 = this.add.text(30, 620, "", {font:"16px Ariel"});
+        this.historyTextLine5 = this.add.text(30, 645, "", {font:"16px Ariel"});
+        this.historyTextLine6 = this.add.text(30, 670, "", {font:"16px Ariel"});
+
+        this.historyLogCount = this.add.text(440, 635, "0 / 0", {font:"12px Ariel"});
+
 
         this.historyLineTextList = [];
         this.historyLineTextColor = [];
+        this.historyTextScroll = 0;
 
-        this.goblin = this.add.sprite(100, 100, 'goblin').setInteractive(); // set interactive allows pointerover event
+        this.goblin = this.add.sprite(100, 300, 'goblin').setInteractive(); // set interactive allows pointerover event
 
         this.goblin.text = this.add.text(20, 0, "", {font:"24px Ariel", color:"Red"});
         this.goblin.name = "Goblin";
@@ -95,6 +140,8 @@ class Scene1 extends Phaser.Scene {
         this.goblin.defense = 2;
         this.goblin.health = 8;
         this.goblin.maxhealth = 8;
+        this.goblin.status = "Alive";
+        this.goblin.respawnTimer = 200;
         
         // this.hero.anchor.setTo(0.5, 0.5);
 
@@ -108,6 +155,7 @@ class Scene1 extends Phaser.Scene {
 
         this.key_LEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
         
+        // Handles click events
         this.input.on('pointerdown', function(event) {
             // Moves player to click location
             this.mouseClickX = event.x;
@@ -123,12 +171,43 @@ class Scene1 extends Phaser.Scene {
             else {
                 this.battleMode = false;
             }
-            console.log("Battle mode :", this.battleMode );
+            // console.log("Battle mode :", this.battleMode );
 
             console.log("x: ", event.x, " y: ", event.y);
-            console.log("player position :", this.hero.x, this.hero.y)
-            console.log("enemy position :", this.goblin.clickPosition)
-            
+            //console.log("player position :", this.hero.x, this.hero.y);
+            // console.log("enemy position :", this.goblin.clickPosition);
+
+            if (this.mouseClickX >= 446 && this.mouseClickX <=485 && this.mouseClickY >= 558 && this.mouseClickY <= 590) {
+                if (this.historyLineTextList.length > 6 && this.historyTextScroll < this.historyLineTextList.length - 5) {
+                    this.historyTextScroll += 1
+                }
+            }
+
+            if (this.mouseClickX >= 446 && this.mouseClickX <=485 && this.mouseClickY >= 598 && this.mouseClickY <= 630) {
+                if (this.historyLineTextList.length > 6 && this.historyTextScroll > 0) {
+                    this.historyTextScroll -= 1
+                }
+            }
+
+            // Attack stance button
+            if (this.mouseClickX >= 540 && this.mouseClickX <=565 && this.mouseClickY >= 522 && this.mouseClickY <= 545) {
+                this.radioButton.x = 552;
+                this.radioButton.y = 530;
+                this.hero.attackStance = "Aggressive";
+            }
+            else if (this.mouseClickX >= 682 && this.mouseClickX <= 707 && this.mouseClickY >= 522 && this.mouseClickY <= 545) {
+                this.radioButton.x = 692;
+                this.radioButton.y = 530;
+                this.hero.attackStance = "Defensive";
+            }
+            else if (this.mouseClickX >= 540 && this.mouseClickX <=565 && this.mouseClickY >= 564 && this.mouseClickY <= 591) {
+                this.radioButton.x = 552;
+                this.radioButton.y = 572;
+                this.hero.attackStance = "Normal";
+            }
+            console.log(this.hero.attackStance);
+
+
         },this);
 
         // Changes scenes
@@ -149,6 +228,7 @@ class Scene1 extends Phaser.Scene {
         // Moves player left while holding left key
         if(this.key_LEFT.isDown) {
             this.hero.x--;
+            main();
         }
 
         // Moves player to mouse click
@@ -188,7 +268,7 @@ class Scene1 extends Phaser.Scene {
         this.changeDirTimer -= 1;
         // console.log(this.battleMode);
 
-        if (this.battleMode == false) {
+        if (this.battleMode == false && this.goblin.status == "Alive") {
             if (this.randNumb == 0 && this.goblin.x < this.sceneX[1]) {
                 this.goblin.x += .5;
             }
@@ -201,6 +281,9 @@ class Scene1 extends Phaser.Scene {
             else if (this.randNumb == 3 && this.goblin.y > this.sceneY[0]) {
                 this.goblin.y -= .5;
             }
+        }
+
+        if (this.battleMode == false) {
             // hides status bars
             this.statusBarHero100.y = -40;
             this.statusBarEnemy100.y= -40;
@@ -208,7 +291,6 @@ class Scene1 extends Phaser.Scene {
             this.statusBarEnemy0.y = -40;
             this.heroDamageText.y = -50;
             this.enemyDamageText.y = -50;
-
         }
 
         if (this.battleMode == true) {
@@ -241,6 +323,21 @@ class Scene1 extends Phaser.Scene {
                 }
                 this.historyLineTextList.unshift(`Hero does ${this.attackPower} damage to ${this.goblin.name}`);
                 this.historyLineTextColor.unshift(this.neon);
+
+                // Experience points
+                if (this.hero.attackStance == "Aggressive") {
+                    this.hero.powerExp += Math.round(this.attackPower * 3.7);
+                    this.hero.healthExp += Math.round(this.attackPower * 1.5);
+                }
+                else if (this.hero.attackStance == "Defensive") {
+                    this.hero.defenseExp += Math.round(this.attackPower * 3.7);
+                    this.hero.healthExp += Math.round(this.attackPower * 1.5);
+                }
+                else if (this.hero.attackStance == "Normal") {
+                    this.hero.powerExp += Math.round(this.attackPower * 1.5);
+                    this.hero.defenseExp += Math.round(this.attackPower * 1.5);
+                    this.hero.healthExp += Math.round(this.attackPower * 2.0);
+                }
             }
             if (this.attackTimer == 90) {
                 this.enemyAttackPower = Math.floor(Math.random() * (this.goblin.power + 1));
@@ -266,12 +363,52 @@ class Scene1 extends Phaser.Scene {
             this.enemyDamageText.y = this.goblin.y - 55;
             this.enemyDamageText.setText(this.attackPower).setStyle({font:"18px Ariel Bold", color: this.damageColor});;
 
-
             if (this.attackTimer <= 0) {
                 this.attackTimer =  200;
             }
         }
+
+        // Character death
+        if (this.goblin.health <= 0 || this.hero.health <= 0) {
+            if (this.goblin.health <= 0) {
+                this.goblin.status = "Dead";
+                this.goblin.y = -50;
+            }
+            else if (this.hero.health <= 0) {
+                this.hero.x = 300;
+                this.hero.y = 300;
+                this.hero.health = this.hero.maxhealth
+            }
+            this.battleMode = false;
+        }
+
+        // Enemy respawn
+        if (this.goblin.status == "Dead") {
+            this.goblin.respawnTimer -= 1;
+            if (this.goblin.respawnTimer <= 0) {
+                this.goblin.status = "Alive";
+                this.goblin.x = 100;
+                this.goblin.y = 300;
+                this.goblin.respawnTimer = 200;
+                this.goblin.health = this.goblin.maxhealth;
+            }
+        }
         
+        // Leveling up
+        if (this.hero.powerExp >= this.hero.nextPowerLevelExp) {
+            this.hero.power += 1;
+            this.hero.nextPowerLevelExp = Math.round(((25 + (this.hero.power + 1)) * (this.hero.power + 1) / 1.13767) * this.hero.power);
+        }
+        if (this.hero.defenseExp >= this.hero.nextDefenseLevelExp) {
+            this.hero.defense += 1;
+            this.hero.nextDefenseLevelExp = Math.round(((25 + (this.hero.defense + 1)) * (this.hero.defense + 1) / 1.13767) * this.hero.defense);
+            
+        }
+        if (this.hero.healthExp >= this.hero.nextHealthLevelExp) {
+            this.hero.health += 1;
+            this.hero.nextHealthLevelExp = Math.round(((25 + (this.hero.health + 1)) * (this.hero.health + 1) / 1.13767) * this.hero.health);
+        }
+
 
         // Mouse over enemy
         
@@ -287,16 +424,22 @@ class Scene1 extends Phaser.Scene {
 
         this.heroPowerLvText.setText(this.hero.power);
         this.heroDefenseLvText.setText(this.hero.defense);
-        this.heroHealthLvText.setText(this.hero.health);
+        this.heroHealthLvText.setText(`${this.hero.health} / ${this.hero.maxhealth}`);
+
+        this.heroPowerExpText.setText(`Current Exp: ${this.hero.powerExp}        Next Level: ${this.hero.nextPowerLevelExp}`);
+        this.heroDefenseExpText.setText(`Current Exp: ${this.hero.defenseExp}        Next Level: ${this.hero.nextDefenseLevelExp}`);
+        this.heroHealthExpText.setText(`Current Exp: ${this.hero.healthExp}        Next Level: ${this.hero.nextHealthLevelExp}`);
 
         // Updates history log
 
-        this.historyTextLine1.setText(this.historyLineTextList[5]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[5]});
-        this.historyTextLine2.setText(this.historyLineTextList[4]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[4]});;
-        this.historyTextLine3.setText(this.historyLineTextList[3]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[3]});;
-        this.historyTextLine4.setText(this.historyLineTextList[2]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[2]});;
-        this.historyTextLine5.setText(this.historyLineTextList[1]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[1]});;
-        this.historyTextLine6.setText(this.historyLineTextList[0]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[0]});;
+        this.historyTextLine1.setText(this.historyLineTextList[this.historyTextScroll + 5]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[this.historyTextScroll + 5]});
+        this.historyTextLine2.setText(this.historyLineTextList[this.historyTextScroll + 4]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[this.historyTextScroll + 4]});
+        this.historyTextLine3.setText(this.historyLineTextList[this.historyTextScroll + 3]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[this.historyTextScroll + 3]});
+        this.historyTextLine4.setText(this.historyLineTextList[this.historyTextScroll + 2]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[this.historyTextScroll + 2]});
+        this.historyTextLine5.setText(this.historyLineTextList[this.historyTextScroll + 1]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[this.historyTextScroll + 1]});
+        this.historyTextLine6.setText(this.historyLineTextList[this.historyTextScroll]).setStyle({font:"16px Ariel Bold", color: this.historyLineTextColor[this.historyTextScroll]});
 
+        this.historyLogCount.setText(`${this.historyLineTextList.length - this.historyTextScroll} / ${this.historyLineTextList.length}`)
+        
     }
 }
